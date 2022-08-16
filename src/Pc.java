1e7eb5f2c0d5cc1265cb7f57d389e82e23281d81
Ibrahim Gana Malgwi <108299271+IbrahimMalgwi@users.noreply.github.com>
2022-08-16 16:16:47 +0100

    new commit

---
+++ src/Pc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Pc {
+    public static void main(String[] args) {
+        String name = "Welcome to 30 Days of Code!";
+
+        System.out.println("Hello, World.");
+        System.out.println(name);
+    }
+}
